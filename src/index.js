4a4379bf0660caa54ceb2ad4cb707bc525a28666
malek2005 <malek2005@list.ru>
2021-03-13 00:35:36 +0200

    feat: solution for typical arrays problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
+  
+    if (array && array.length) 
+    {
+      let min = Math.min.apply(null, array);
+      return min;
+    
+  } 
+  else {
+  
   return 0;
+ 
+  }
 }
-
+  
 exports.max = function max (array) {
+  
+  if (array && array.length)
+  
+  {
+    let max = Math.max.apply(null, array);
+    return max;
+    
+  } 
+  else 
+  {
   return 0;
+  
+}
 }
 
 exports.avg = function avg (array) {
+    if (array && array.length)
+     {
+       return array.reduce((a, b) => (a + b)) / array.length;
+    
+  }
+  else {
   return 0;
+  }
+
 }
+
